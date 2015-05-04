841e59d1b27687c78ee122d88db50421582fd8bc
Alexis Algoud <alexis@atm-consulting.fr>
2015-05-04 15:46:14 +0200

    NEW verify stock

---
+++ script/verify-stock.php
@@ -0,0 +1,36 @@
+<?php
+
+	define('INC_FROM_CRON_SCRIPT',true);
+	
+	require('../config.php');
+
+	$fk_entrepot = GETPOST('fk_entrepot');
+	
+	if(empty($fk_entrepot)) exit('fk_entrepot ?');
+
+	$PDOdb=new TPDOdb;
+	$Tab = $PDOdb->ExecuteAsArray("SELECT fk_product FROM ".MAIN_DB_PREFIX."product_stock WHERE fk_entrepot=".(int)$fk_entrepot);
+
+
+	dol_include_once('/product/class/product.class.php');
+
+	$TStock=array();
+
+	foreach($Tab as $row ) {
+
+		$product=new Product($db);
+		$product->fetch($row->rowid);
+		
+		$product->load_stock();
+		
+		$stock = $product->stock_warehouse[$fk_entrepot]->real;
+	
+		$TStock[]= array(
+			'ref'=>$product->ref
+			,'fk_entrepot'=>$fk_entrepot
+			,'stock'=>$stock
+		)	;
+	}
+	
+	
+	var_dump($TStock);
